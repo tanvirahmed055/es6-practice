2efb0ca06a56b3ecf2bb072e44d81fd37e301872
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-08-25 03:34:29 +0600

    v3

--- practice.js
+++ practice.js
@@ -1,8 +1,4 @@
 /*
-1. let const
-2. template string
-2.a: use variable
-2.b: use object  property
 3. arrow function
 3.a. with one parameter. will return the number after dividing by 5
 3.b. with two parameters. will add two to each number and then multiply the result.
@@ -29,6 +25,8 @@ oilPrice = 900;
 
 
 //2. template string
+//2.a: use variable
+//2.b: use object  property
 //২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। 
 
 const xiomiPhone = {
@@ -39,3 +37,15 @@ const xiomiPhone = {
 const description = `Hi! My first name is ${firstName}. I have oil in my shopping list and it costs me ${oilPrice} per litre. My friend has bought a ${xiomiPhone.model} phone which costs around ${xiomiPhone.price} taka.`;
 console.log(description);
 
+
+
+
+//3. arrow function
+
+//3.a. with one parameter. will return the number after dividing by 5
+//৩.১ একটা প্যারামিটার ওয়ালা arrow ফাংশন ডিক্লেয়ার করো। এবং সেই ফাংশনের কাজ হবে তুমি কোন ইনপুট দিলে সেই ইনপুট সংখ্যাকে ৫ দিয়ে ভাগ করে আউটপুট দিবে। 
+
+
+const divideByFive = num => num / 5;
+const result = divideByFive(25);
+console.log(result);
